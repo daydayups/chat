86b73cd6cd77965c958f46cdaba74c8810029280
lijin0112 <gaizhi00@gmail.com>
2016-01-13 14:37:32 +0800

    fun: Add private messaging

--- index.js
+++ index.js
@@ -5,28 +5,23 @@ var io = require('socket.io')(http);
 
 app.get('/', function (req, res) {
   res.sendFile(__dirname + '/index.html');
-//	res.sendFile('index.html');
 });
 app.use(express.static('public'));
-/*
- io.on('connection', function(socket){
- socket.on('chat message', function(msg){
- console.log('message: ' + msg);
- });
- });
- */
-var online = {};
+
+var online_user = {};
+var user_socket = {};
 io.on('connection', function (socket) {
   var uid = Math.floor(Math.random() * 10000);
-  online[socket.id] = uid;
-  io.emit('connect status', 'user ' + online[socket.id] + ' connected', online);
+  online_user[socket.id] = uid;
+  user_socket[socket.id] = socket;
+  io.emit('connect status', 'user ' + online_user[socket.id] + ' connected', online_user);
 
   //socket.broadcast.emit('user connected');
 
   socket.on('disconnect', function () {
-    var tmp = online[socket.id];
-    delete online[socket.id];
-    io.emit('connect status', 'user ' + tmp + ' disconnected', online);
+    var tmp = online_user[socket.id];
+    delete online_user[socket.id];
+    io.emit('connect status', 'user ' + tmp + ' disconnected', online_user);
   });
 
   socket.on('typing', function (from) {
@@ -35,16 +30,21 @@ io.on('connection', function (socket) {
 
   socket.on('chat message', function (msg) {
     //io.emit('chat message', from +' : '+msg);
-    socket.broadcast.emit('chat message', online[socket.id], msg);
+    socket.broadcast.emit('chat message', online_user[socket.id], msg);
   });
 
   socket.on('modify uid', function (oldname, nickname) {
-    online[socket.id] = nickname;
-    //delete online[socket.id];
-    //online[socket.id]=uid;
-    io.emit('chat message', online[socket.id], 'user ' + oldname + ' modified his nickname to ' + nickname)
-    io.emit('online refresh', online);
+    online_user[socket.id] = nickname;
+    //delete online_user[socket.id];
+    //online_user[socket.id]=uid;
+    io.emit('chat message', online_user[socket.id], 'user ' + oldname + ' modified his nickname to ' + nickname)
+    io.emit('online refresh', online_user);
   });
+
+  socket.on('private message', function (s_id, msg) {
+    user_socket[s_id].emit('chat message', "(" + online_user[socket.id] + " 悄悄话 我)", msg);
+  });
+
 });
 
 
